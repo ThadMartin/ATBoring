46261f46a7775fd932dce876ddfc91bf566c3f3d
Thad Martin <martin_thad@yahoo.com>
2012-09-12 17:03:47 -0400

    Initial Commit

---
+++ AT_Boring/AT_BoringAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AT_BoringAppDelegate.h
+//  AT_Boring
+//
+//  Created by Thad Martin on 9/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AT_BoringViewController;
+
+@interface AT_BoringAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) AT_BoringViewController *viewController;
+
+@end
---
+++ AT_Boring/AT_BoringViewController.h
@@ -0,0 +1,13 @@
+//
+//  AT_BoringViewController.h
+//  AT_Boring
+//
+//  Created by Thad Martin on 9/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AT_BoringViewController : UIViewController
+
+@end
